41fd1c3044558856fd0c24ce425e2500f9970722
kikkipedia <49567322+kikkipedia@users.noreply.github.com>
2026-04-02 11:40:55 +0200

    first commit

---
+++ src/views/MapView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="map" style="height: 100vh;">
+    <Map />
+  </div>
+</template>
+
+<script setup>
+import Map from '@/components/Map.vue';
+</script>
+
+<style scoped>
+
+
+</style>
